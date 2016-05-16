bc228b668a8bb3f28ce060b52a602f56f2a94271
JamesWinestock <james.winestock@gmail.com>
2016-05-16 16:56:58 +1000

    show products on home page, works but looks crap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516051057) do
+ActiveRecord::Schema.define(version: 20160516062847) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 20160516051057) do
   end
 
   add_index "accounts", ["user_id"], name: "index_accounts_on_user_id"
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -34,7 +47,10 @@ ActiveRecord::Schema.define(version: 20160516051057) do
     t.boolean  "available"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
+
+  add_index "products", ["slug"], name: "index_products_on_slug", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Product.delete.all
+# Product.create(name: 'One M8', price: 200.0, brand: 'HTC')
+
+products = [
+  {name: 'One M8', price: 200.0, brand: 'HTC'},
+  {name: 'One M9', price: 300.0, brand: 'HTC'},
+  {name: 'Ten', price: 500.0, brand: 'HTC'},
+
+]
+
+products.each do |product|
+  Product.create(product)
+end
+
+user = User.create(email: 'admin2@example.com', password: '123456789')
+account = Account.create(user_id: user.id, shipping_address: '7 Kelly street', phone: '1234567890')
